813ab8da0c61e3595edd7f57fd0b0a787afe688b
izszzz <izszzz@.yahoo.co.jp>
2021-02-17 07:32:37 +0900

    add Navigation Script

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string _sceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Navigate()
+    {
+        SceneManager.LoadScene (_sceneName);
+    }
+}
